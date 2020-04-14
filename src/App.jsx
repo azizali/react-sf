77934061eb52a5659439762458c3e6c3a3f852b4
Aziz Ali <aziz@ilovecoding.org>
2020-04-14 13:28:41 -0500

    Class based component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-// import Header from './Header';
+import Header from './Header';
+import HeaderClass from './HeaderClass';
 // import ContentTwo from './ContentTwo';
 // import Box from './Box';
 // import Footer from './Footer';
@@ -7,6 +8,12 @@ import CarsList from './CarsList';
 
 export default function App (props) {
   return (
-    <CarsList />
+    <>
+      <Header companyName="SF" tagLine="We are the geeks" />
+      <HeaderClass
+        companyName="SF2"
+        tagLine="We are the geeks"
+      />
+    </>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default function Header (){
+export default function Header (props){
   return (
-    <div>Header</div>
+    <div>Header: {props.companyName}</div>
   )
 }
